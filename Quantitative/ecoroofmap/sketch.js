0fae6495ac9633afd201888b0f4ee4be2a6e3ed2
mbcarradine <carradim@newschool.edu>
2017-02-20 20:17:44 -0500

    20140220

--- Quantitative/ecoroofmap/sketch.js
+++ Quantitative/ecoroofmap/sketch.js
@@ -27,7 +27,7 @@ function drawDataPoints(){
   // cycle through array
   //console.log(latitudes[0]);
     for(var i=0; i<500; i++){
-      var col = 'green';
+      var col = (185,216,57);
       if(rooftype[i]==="Cool"){
         col = 'white';
       }else if(rooftype[i]==="Blue"){
