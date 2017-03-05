4bdab62e39bffeae9ba356d542a37a25f609d109
mbcarradine <carradim@newschool.edu>
2017-03-05 14:24:28 -0500

    20170305

--- practice/p5 max dabbles/sketch.js
+++ practice/p5 max dabbles/sketch.js
@@ -1,32 +1,21 @@
-var x = 0;
-var speed = 10;
-var col = {
-r: 255,
-g: 0,
-b: 0
-};
+var quotes;
+
+function preload() {
+
+quotes = loadJSON("quotedata.json");
+}
+
 
 function setup() {
- 	createCanvas(600,600); 
-
+	createCanvas(600,600);
 
 
 }
 
 function draw() {
-  col.r = random(0,255);
-  col.g = random(0,255);
-  col.b = random(0,255);
-background(50);
-stroke(255);
-strokeWeight(4);
-ellipse(x, 200, 50,50);
-if (x > width || x < 0){
-speed = speed * -1;
-fill(col.r,col.g,col.b);
-}
-x = x + speed
-
-
+	background(0);
+	fill(quotes .r, quotes.g, quotes.b);
+	text(quotes.quote, 10, 50);
 
 }
+
