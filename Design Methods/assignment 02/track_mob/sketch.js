eff389ae24166a0e242611b13bcdd469b9c37388
MBCarradine <carradim@newschool.edu>
2017-09-24 18:46:13 -0400

    20170924

--- Design Methods/assignment 02/track_mob/sketch.js
+++ Design Methods/assignment 02/track_mob/sketch.js
@@ -11,9 +11,9 @@ function setup() {
 
 // LEAFLET CODE
 
-mymap = L.map('mapid').setView([40.7354989, -73.9918813], 16);
+mymap = L.map('mapid').setView([40.7354989, -73.9918813], 17.4);
   L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWJjYXJyYWRpbmUiLCJhIjoiY2l6ZWxmeGh6MjJicjJ2cW9ncDh3ZzVpMiJ9.QMaUpzAhGuRXJZcP9-TtdQ', {
-  maxZoom: 20,
+  maxZoom: 22,
   id: 'mapbox.streets'
   }).addTo(mymap);
   drawDataPoints();
@@ -32,34 +32,22 @@ function drawDataPoints(){
   date = table.getColumn("Date");
 
     for(var i=0; i<25; i++){
-      var col = 'black';
+      var col = '#161414';
       if(gender[i]==="M"){
-        col = 'deepskyblue';
+        col = '#5B809D';
       }else if(gender[i]==="F"){
-        col= 'red';
+        col= '#EF4F4E';
       }
 
       var circle = L.circle([lat[i], long[i]], {
-        radius: 30,
+        radius: 15,
         stroke: false,
         fillColor: col,
-        fillOpacity: 0.5
+        fillOpacity: .8
       }).addTo(mymap)
       .bindPopup(address[i] + " on " + day[i] + " " + date[i] + " at " + time[i] + ". " + desc[i]);
   }
 }
 
-// function draw() {
-//     textSize(12);
-// fill ('gray');
-// rect (25,200,10,10);
-// text ("IDK", 40, 210);
-// fill ('deepskyblue');
-// rect (25,225,10,10);
-// text ("Male", 40, 235);
-// fill ('red');
-// rect (25, 250,10,10);
-// text ("Female", 40, 260);
 
-// }
 
