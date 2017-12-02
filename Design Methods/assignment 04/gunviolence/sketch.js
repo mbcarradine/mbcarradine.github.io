1f50bfde6b5606639813544e76157b247eb8190d
MBCarradine <carradim@newschool.edu>
2017-12-02 02:32:47 -0500

    20171201

--- Design Methods/assignment 04/gunviolence/sketch.js
+++ Design Methods/assignment 04/gunviolence/sketch.js
@@ -73,7 +73,7 @@ textAlign(RIGHT)
 text ('Sources', windowWidth - 50, windowHeight - 65);
 text ('@NRA Twitter', windowWidth - 50, windowHeight - 50);
 text ('The Gun Violence Archive', windowWidth - 50, windowHeight - 35);
-text ('Last updated: Nov 28, 2017 at 11:25 EST', windowWidth - 50, windowHeight - 20);
+text ('Last updated: Dec 2, 2017 at 02:25 EST', windowWidth - 50, windowHeight - 20);
 
 textAlign(LEFT)
 frameRate(10);
