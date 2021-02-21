bacb4cc5d08b3e2f6911cc1a2e1a7d73aceab88d
MBCarradine <carradim@newschool.edu>
2021-02-21 16:53:23 -0500

    20210221

---
+++ Design Methods/assignment 04/gunviolence/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+ consumer_key:         'm6IN0eKkWXFRL09xUPmasn3cI',
+  consumer_secret:      '98VZhVxwpg5JNajKT6L2yMBsjoc8ilvel30JRAVahswl2Xl2lQ',
+  access_token:         '919303063558590464-JjTjaNZVYTmQKsLtkqh68fb4s64pNJP',
+  access_token_secret:  'SlFmhQ86CGl0BTPJvK0icLCb6TvbydonCZaAG0zcRgIXH'
+
+}
